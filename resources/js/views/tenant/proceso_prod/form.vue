09abbcb9b0345627e206bb84d263b00fcb12d1f0
marlon20161188f <mavilat@uni.pe>
2022-04-08 06:31:36 -0500

    Update form.vue

--- resources/js/views/tenant/proceso_prod/form.vue
+++ resources/js/views/tenant/proceso_prod/form.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="card mb-0 pt-2 pt-md-0">
         <div class="card-header bg-info">
-            <h3 class="my-0">Nueva producción</h3>
+            <h3 class="my-0">Nueva produccións</h3>
         </div>
         <div class="tab-content">
             <form autocomplete="off" @submit.prevent="submit"
